156294566d59f6efd1f6bbbe90bdb9555929237d
MehmetCanBOZ <bozmehmetcan01@gmail.com>
2023-09-16 15:21:21 +0300

    feat: implement router object

--- services/Router.js
+++ services/Router.js
@@ -1,10 +1,52 @@
 const Router = {
   init: () => {
     document.querySelectorAll("a.navlink").forEach((a) => {
-      a.addEventListener("click", (event) => {});
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+
+    //event handler for url changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
     });
   },
-  go: (route, addToHistory = true) => {},
+  go: (route, addToHistory = true) => {
+    console.log("going to :", route);
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "order";
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          const paramId = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+
+    if (pageElement) {
+      const main = document.querySelector("main");
+      main.innerHTML = "";
+      main.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
+  },
 };
 
 export default Router;
